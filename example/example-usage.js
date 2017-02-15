85a841b7e7198cdbee4d3e57d482b49e8d1fb368
Taka Okunishi <okunishinish@gmail.com>
2017-02-15 16:54:24 +0900

    いんちき認証をライブラリ化した

--- example/example-usage.js
+++ example/example-usage.js
@@ -1,3 +1,15 @@
 'use strict'
 
 const sgGuard = require('sg-guard')
+
+{
+  const ThePassword = 'BigBigApple'
+
+  let guard = sgGuard(ThePassword)
+
+  // Ask use to input password
+  let ok = guard.ask()
+  if (!ok) {
+    throw new Error('Auth failed!')
+  }
+}
